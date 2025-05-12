ab358185d7b893661e6748e9bc7cf24313d456b6
Manideep3101 <bmanideepreddy3101@gmail.com>
2025-05-12 18:36:20 +0530

    modified ui screens

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
+import "pages/Student/student_dashboard.dart";
+// import "pages/Admin/admin_dashboard.dart";
 
-import "pages/Admin/admin_dashboard.dart";
-
-void main() => runApp(AdminApp());
+// void main() => runApp(AdminApp());
+void main() => runApp(StudentApp());
